c8000c080f1193670aa323034db37cce2d59fb62
chenxqiyu <390739790@qq.com>
2022-04-13 15:07:48 +0800

    add

--- test.js
+++ test.js
@@ -1,18 +1,31 @@
 
 
-var client = () => {
+var client = function () {
     this.x = 0
     this.y = 0
 
 }
-client.prototype.start = () => {
+client.prototype.start = function () {
+    this.x = 123
+    this.y = 456
     console.log("start")
+}
+
+class client2 {
+    constructor() {
+        this.xx = 213
+        this.yy = 456
+    }
+    start() {
+        console.log("start2")
+    }
 }
 
 window.onload = function () {
 
     var c = new client()
     c.start()
-
+    var c2=new client2()
+    c2.start()
 
 }
