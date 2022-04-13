bcaae5de06faeb6b04018326f000fa21c29667ed
chenxqiyu <390739790@qq.com>
2022-04-13 17:27:25 +0800

    add

--- test.js
+++ test.js
@@ -1,31 +1,113 @@
+class client {
+    constructor(no) {
+        this.no = no
+        this.x = 0
+        this.y = 0
+        this.alive = true
 
-
-var client = function () {
-    this.x = 0
-    this.y = 0
-
-}
-client.prototype.start = function () {
-    this.x = 123
-    this.y = 456
-    console.log("start")
-}
-
-class client2 {
-    constructor() {
-        this.xx = 213
-        this.yy = 456
     }
-    start() {
-        console.log("start2")
+    change(move) {
+        this.y += move;
     }
+    update() {
+        if (this.y > window.outerHeight) { this.y = 0; } else { this.y++ }
+
+    }
+
 }
 
+var me = []
+var gen = []
+//神经网络-创建神经网络
+var ne = new Neuroevolution({
+    population: 50,// 代数
+    network: [2, [2], 1],// 感知器结构
+});
+function start() {
+
+    me = []
+    //开始生成
+    gen = ne.nextGeneration();
+    console.log(gen);
+
+    for (var i = 0; i < gen.length; i++) {
+        me.push(new client(i))
+    }
+    console.log(me)
+
+}
 window.onload = function () {
 
-    var c = new client()
-    c.start()
-    var c2=new client2()
-    c2.start()
+
+    start()
+
+    setInterval(function () {
+        var nextHoll = 0;
+
+        nextHoll++
+
+        for (var i in me) {
+            if (me[i].alive) {
+
+                //输入
+                var inputs =
+                    [
+                        me[i].y,
+                        nextHoll
+                    ];
+
+                var res = gen[i].compute(inputs);
+                if (res > 0.5) {
+                    me[i].change(Math.round(Math.random() * window.outerHeight))
+                }
+
+
+                //输出
+
+                //得分
+                if (me[i].y > window.outerHeight) {
+                    me[i].alive = false
+                    ne.networkScore(this.gen[i], this.score);
+                    var isall = true;
+                    for (var id in me) {
+                        if (me[id].alive) {
+                            isall = false
+                            return false
+                        }
+                    }
+                    if (isall) {
+                        start()
+                    }
+                }
+                //重启
+                console.log(me[i].no);
+            }
+        }
+    }, 1000)
+    setInterval(function () {
+        document.body.innerHTML = ""
+        for (var i in me) {
+            if (me[i].alive) {
+                document.write(me[i].no + ",")
+            }
+        }
+        document.write("<br>")
+        for (var i in me) {
+            if (!me[i].alive) {
+                document.write(me[i].no + ",")
+            }
+        }
+    }, 1000)
+
+
+    //输入
+    //输出
+    //>0.5移动
+
+    //满足条件得分
+    //  networkScore(this.gen[i], this.score);
+
+    //如果对象组为0重置
+
 
 }
